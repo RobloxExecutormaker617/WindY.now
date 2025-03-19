7e6b1b40769fb0c37748a8b83b3ebd6d748e6430
RobloxExecutormaker617 <naseebnoveera0@gmail.com>
2025-03-19 01:47:37 -0700

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -7,16 +7,16 @@ canvas.height = window.innerHeight;
 
 const letters = Array(256).join(1).split('');
 
-function draw() {
+function drawMatrix() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#00ff00';
+    ctx.fillStyle = '#0F0';
     letters.map((y_pos, index) => {
-        const text = String.fromCharCode(65 + Math.random() * 33);
+        const text = String.fromCharCode(3e4 + Math.random() * 33);
         const x_pos = index * 10;
         ctx.fillText(text, x_pos, y_pos);
         letters[index] = y_pos > 758 + Math.random() * 1e4 ? 0 : y_pos + 10;
     });
 }
 
-setInterval(draw, 33);
+setInterval(drawMatrix, 33);
